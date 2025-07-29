6d29368b6d49c085f9d9a29ea09508f75633e847
slyzOwO <lilithhimes@gmail.com>
2025-07-29 11:26:47 -0500

    Initial commit

---
+++ src/main/java/net/slyzowo/moremetals/Moremetals.java
@@ -0,0 +1,15 @@
+package net.slyzowo.moremetals;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Moremetals implements ModInitializer {
+	public static final String MOD_ID = "moremetals";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/net/slyzowo/moremetals/MoremetalsClient.java
@@ -0,0 +1,10 @@
+package net.slyzowo.moremetals;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MoremetalsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
