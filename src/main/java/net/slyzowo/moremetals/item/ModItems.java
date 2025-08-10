7c769ef0e7a7365dd29b38ab90f6b5dac8c03977
slyzOwO <lilithhimes@gmail.com>
2025-08-10 18:21:25 -0500

    0.0.2A adds more metals, no textures tho

--- src/main/java/net/slyzowo/moremetals/item/ModItems.java
+++ src/main/java/net/slyzowo/moremetals/item/ModItems.java
@@ -12,12 +12,28 @@ public class ModItems {
 
 
     public static final Item ALUMINIUM_INGOT = registerItem("aluminium_ingot", new Item(new Item.Settings()));
+    public static final Item ALUMINIUM_NUGGET = registerItem("aluminium_nugget", new Item(new Item.Settings()));
+
     public static final Item BRASS_INGOT = registerItem("brass_ingot", new Item(new Item.Settings()));
+    public static final Item BRASS_NUGGET = registerItem("brass_nugget", new Item(new Item.Settings()));
+
     public static final Item BRONZE_INGOT = registerItem("bronze_ingot", new Item(new Item.Settings()));
+    public static final Item BRONZE_NUGGET = registerItem("bronze_nugget", new Item(new Item.Settings()));
+
     public static final Item STEEL_INGOT = registerItem("steel_ingot", new Item(new Item.Settings()));
+    public static final Item STEEL_NUGGET = registerItem("steel_nugget", new Item(new Item.Settings()));
+
     public static final Item TIN_INGOT = registerItem("tin_ingot", new Item(new Item.Settings()));
+    public static final Item TIN_NUGGET = registerItem("tin_nugget", new Item(new Item.Settings()));
+
+    public static final Item TITANIUM_INGOT = registerItem("titanium_ingot", new Item(new Item.Settings()));
+    public static final Item TITANIUM_NUGGET = registerItem("titanium_nugget", new Item(new Item.Settings()));
+
     public static final Item PURPLE_GOLD_INGOT = registerItem("purple_gold_ingot", new Item(new Item.Settings()));
+    public static final Item PURPLE_GOLD_NUGGET = registerItem("purple_gold_nugget", new Item(new Item.Settings()));
+
     public static final Item ZINC_INGOT = registerItem("zinc_ingot", new Item(new Item.Settings()));
+    public static final Item ZINC_NUGGET = registerItem("zinc_nugget", new Item(new Item.Settings()));
 
 
     private static Item registerItem(String name, Item item){
@@ -29,12 +45,21 @@ public class ModItems {
 
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
             entries.add(ALUMINIUM_INGOT);
+            entries.add(ALUMINIUM_NUGGET);
             entries.add(BRASS_INGOT);
+            entries.add(BRASS_NUGGET);
             entries.add(BRONZE_INGOT);
+            entries.add(BRONZE_NUGGET);
             entries.add(STEEL_INGOT);
+            entries.add(STEEL_NUGGET);
             entries.add(TIN_INGOT);
+            entries.add(TIN_NUGGET);
+            entries.add(TITANIUM_INGOT);
+            entries.add(TITANIUM_NUGGET);
             entries.add(PURPLE_GOLD_INGOT);
+            entries.add(PURPLE_GOLD_NUGGET);
             entries.add(ZINC_INGOT);
+            entries.add(ZINC_NUGGET);
         });
     }
 }
